7603e324ac242f29f129ba9d91b907fc5f731eec
Ayush <aks010598@gmail.com>
2019-04-07 11:31:33 +0530

    created superuser and table 'post'

--- blogApp/models.py
+++ blogApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    author=models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    title=models.CharField(max_length=100)
+    text=models.TextField()
+    published_date=models.DateTimeField(default=timezone.now)
