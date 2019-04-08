6aeb085349f1a4474a6605eac93355a864671bad
Ayush <aks010598@gmail.com>
2019-04-08 22:24:59 +0530

    post modified

--- blogApp/models.py
+++ blogApp/models.py
@@ -7,3 +7,6 @@ class Post(models.Model):
     title=models.CharField(max_length=100)
     text=models.TextField()
     published_date=models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
